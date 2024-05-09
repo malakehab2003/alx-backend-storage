20883e2ff4b9ec7d70a1206e109ff9007619b696
root <lokaehab2003@gmail.com>
2024-05-09 19:34:03 +0000

    task 10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,13 @@
+-- create a function to return a / b
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    DECLARE result FLOAT DEFAULT 0;
+
+    IF b != 0 THEN
+        SET result = a / b;
+    END IF;
+    return result;
+END $$
+DELIMITER ;
