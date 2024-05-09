a56662310eb54466369bd02ca508bd4d1e9ba8dd
root <lokaehab2003@gmail.com>
2024-05-09 22:54:39 +0000

    task 101

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,3 +1,4 @@
+-- creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
 BEGIN
