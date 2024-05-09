d41fdc3425ccd4b35ba9196959b631f419d07005
root <lokaehab2003@gmail.com>
2024-05-09 22:53:19 +0000

    task 101

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,32 @@
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
+BEGIN
+    ALTER TABLE users ADD weight_score INT NOT NULL;
+    ALTER TABLE users ADD wght INT NOT NULL;
+
+    UPDATE users
+        SET weight_score = (
+            SELECT SUM(corrections.score * projects.weight)
+            FROM corrections
+                INNER JOIN projects
+                    ON corrections.project_id = projects.id
+            WHERE corrections.user_id = users.id
+            );
+
+    UPDATE users
+        SET wght = (
+            SELECT SUM(projects.weight)
+                FROM corrections
+                    INNER JOIN projects
+                        ON corrections.project_id = projects.id
+                WHERE corrections.user_id = users.id
+            );
+
+    UPDATE users
+        SET users.average_score = IF(users.wght = 0, 0, users.weight_score / users.wght);
+    ALTER TABLE users
+        DROP COLUMN weight_score;
+    ALTER TABLE users
+        DROP COLUMN wght;
+END $$
+DELIMITER ;
