df0299b5fba4b21aabb216eaa92eded043511ae1
root <lokaehab2003@gmail.com>
2024-05-09 12:08:38 +0000

    task 4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,11 @@
+-- decreases the quantity of an item after adding a new order
+DELIMITER $$
+CREATE TRIGGER update_quantity
+AFTER INSERT ON orders
+FOR Each Row
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
