6e117d06b0ca3e1fe5ccf876642d9c5952e58410
malakehab2003 <lokaehab2003@gmail.com>
2024-05-16 01:02:44 +0300

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,3 +17,32 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    
+    def get(self, key: str, fn: typing.Callable) -> typing.Union[str, bytes, int, float]:
+        """ get function to change the returned datatype """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is None:
+            return value
+        return fn(value)
+    
+    def get_str(self, key: str) -> str:
+        """ return string """
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """ return int """
+        return self.get(key, fn=int)
+    
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
